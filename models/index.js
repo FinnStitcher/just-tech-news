e3fd05aa4034b821c2613b13de2a0a4f53ca21c6
Finn Stitcher <foundfootage@protonmail.com>
2022-07-28 14:45:28 -0700

    vote model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // creating associations between models (tables)
 // matched pairs are necessary
+
+// users and posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
@@ -10,7 +13,36 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+// users and posts thru vote
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// user to votes
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+// post to votes
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
 module.exports = {
     User,
-    Post
+    Post,
+    Vote
 };
