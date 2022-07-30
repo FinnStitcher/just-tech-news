98306909abefe24b0a60e4890e0a4618b9585269
Finn Stitcher <foundfootage@protonmail.com>
2022-07-29 22:04:04 -0700

    comment model

--- models/index.js
+++ models/index.js
@@ -1,48 +1,74 @@
-const User = require('./User');
-const Post = require('./Post');
-const Vote = require('./Vote');
+const User = require("./User");
+const Post = require("./Post");
+const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 // creating associations between models (tables)
 // matched pairs are necessary
 
-// users and posts
+
+// relationships of User
 User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
+	foreignKey: "user_id",
 });
 
-// users and posts thru vote
 User.belongsToMany(Post, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'user_id'
-});
-Post.belongsToMany(User, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'post_id'
+	through: Vote,
+	as: "voted_posts",
+	foreignKey: "user_id",
 });
 
-// user to votes
-Vote.belongsTo(User, {
-    foreignKey: 'user_id'
-});
 User.hasMany(Vote, {
-    foreignKey: 'user_id'
+	foreignKey: "user_id",
 });
 
-// post to votes
-Vote.belongsTo(Post, {
-    foreignKey: 'post_id'
+User.hasMany(Comment, {
+	foreignKey: "user_id",
 });
+
+
+// relationships of Post
+Post.belongsTo(User, {
+	foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+	through: Vote,
+	as: "voted_posts",
+	foreignKey: "post_id",
+});
+
 Post.hasMany(Vote, {
-    foreignKey: 'post_id'
+	foreignKey: "post_id",
+});
+
+Post.hasMany(Comment, {
+	foreignKey: "post_id",
+});
+
+
+// relationships of Comment
+Comment.belongsTo(User, {
+	foreignKey: `user_id`,
+});
+
+Comment.belongsTo(Post, {
+	foreignKey: "post_id",
+});
+
+
+// relationships of Vote
+Vote.belongsTo(User, {
+	foreignKey: "user_id",
+});
+
+Vote.belongsTo(Post, {
+	foreignKey: "post_id",
 });
 
 module.exports = {
-    User,
-    Post,
-    Vote
+	User,
+	Post,
+	Vote,
+	Comment,
 };
